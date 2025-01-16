3ff8b85f2e710aaeeb2b0f941f3647c3fa4ca677
Zach <unixbird@racky.systems>
2025-01-16 09:44:32 +0000

    Update vars.tf with new variable
    added datacenter as an option to switch between locations

--- vars.tf
+++ vars.tf
@@ -1,20 +1,29 @@
+#the name you want the server to be
 variable "server_name" {
-    default = "tiger"
+    default = "test"
     type = string
 }
 
+#lots of possible server types refer to this link: https://www.hetzner.com/cloud/#pricing
 variable "server_type" {
-    default = "cx11"
+    default = "cx22"
     type = string
 }
 
+# possible images: Ubuntu xx.xx, Fedora xx, Debian xx, Centos Stream xx, Rocky Linux x, AlmaLinux x, openSUSE xx
 variable "image_name" {
-    default = "ubuntu-22.04"
+    default = "ubuntu-24.04"
     type = string
 }
 
+#possible datacenters: nbg1-dc3 (Nuremberg), hel1-dc2 (Helsinki), fsn1-dc14 (Falkenstein), ash-dc1 (Ashburn), hil-dc1 (Hillsboro), sin-dc1 (Singapore)
+variable "datacenter" {
+    default = "fsn1-dc14"
+    type = string
+}
 
+#your ssh key inside of hetzner
 variable "ssh_key" {
-    default = "id_hetzner"
+    default = "sshkey"
     type = string
 }
